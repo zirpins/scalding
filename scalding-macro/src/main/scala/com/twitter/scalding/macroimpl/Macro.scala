323209b7afcec1203202618e29cf1bb9247e3a01
Jonathan Coveney <jcoveney@twitter.com>
2014-09-05 17:01:42 -0400

    Take into account better reflection API practices

--- scalding-macro/src/main/scala/com/twitter/scalding/macroimpl/Macro.scala
+++ scalding-macro/src/main/scala/com/twitter/scalding/macroimpl/Macro.scala
@@ -9,60 +9,58 @@ import com.twitter.scalding._
 
 object Macro {
   def caseClassTupleSetter[T]: TupleSetter[T] = macro caseClassTupleSetterImpl[T]
-  def caseClassTupleSetterImpl[T](c: Context)(implicit tag: c.WeakTypeTag[T]): c.Expr[TupleSetter[T]] = {
+  def caseClassTupleSetterImpl[T](c: Context)(implicit T: c.WeakTypeTag[T]): c.Expr[TupleSetter[T]] = {
     import c.universe._
     if (!isCaseClass[T](c)) {
-      throw new IllegalArgumentException("Type paramter of caseClassTupleSetter must be a case class")
+      throw new IllegalArgumentException("Type parameter of caseClassTupleSetter must be a case class")
     }
-    val params = tag.tpe.declaration(nme.CONSTRUCTOR).asMethod.paramss.head
     val set =
-      params
+      T.tpe.declarations
+        .collect { case m: MethodSymbol if m.isCaseAccessor => m }
         .zipWithIndex
         .map {
-          case (sym, idx) =>
-            val name = newTermName(sym.name.toString)
-            sym.typeSignature match {
-              case tpe if tpe =:= typeOf[String] => q"""tup.setString(${idx}, t.$name)"""
-              case tpe if tpe =:= typeOf[Boolean] => q"""tup.setBoolean(${idx}, t.$name)"""
-              case tpe if tpe =:= typeOf[Short] => q"""tup.setShort(${idx}, t.$name)"""
-              case tpe if tpe =:= typeOf[Int] => q"""tup.setInteger(${idx}, t.$name)"""
-              case tpe if tpe =:= typeOf[Long] => q"""tup.setLong(${idx}, t.$name)"""
-              case tpe if tpe =:= typeOf[Float] => q"""tup.setFloat(${idx}, t.$name)"""
-              case tpe if tpe =:= typeOf[Double] => q"""tup.setDouble(${idx}, t.$name)"""
-              //case tpe if tpe.typeSymbol.asInstanceOf[ClassSymbol].isCaseClass => q"""tup.set(${idx}, caseClassTupleSetter[${tpe}](t.$name))"""
-              case tpe if tpe.typeSymbol.asClass.isCaseClass => q"""tup.set(${idx}, caseClassTupleSetter[${tpe}](t.$name))"""
-              case _ => q"""tup.set(${idx}, t.$name)"""
+          case (m, idx) =>
+            m.returnType match {
+              case tpe if tpe =:= typeOf[String] => q"""tup.setString(${idx}, t.$m)"""
+              case tpe if tpe =:= typeOf[Boolean] => q"""tup.setBoolean(${idx}, t.$m)"""
+              case tpe if tpe =:= typeOf[Short] => q"""tup.setShort(${idx}, t.$m)"""
+              case tpe if tpe =:= typeOf[Int] => q"""tup.setInteger(${idx}, t.$m)"""
+              case tpe if tpe =:= typeOf[Long] => q"""tup.setLong(${idx}, t.$m)"""
+              case tpe if tpe =:= typeOf[Float] => q"""tup.setFloat(${idx}, t.$m)"""
+              case tpe if tpe =:= typeOf[Double] => q"""tup.setDouble(${idx}, t.$m)"""
+              case tpe if isCaseClassType(c)(tpe) => q"""tup.set(${idx}, caseClassTupleSetter[${tpe}](t.$m))"""
+              case _ => q"""tup.set(${idx}, t.$m)"""
             }
         }
-        .foldLeft(q"") { (cum, next) => q"""$cum;$next""" }
 
     val res = q"""
-    new _root_.com.twitter.scalding.TupleSetter[${tag.tpe}] {
-      override def apply(t: ${tag.tpe}): _root_.cascading.tuple.Tuple = {
-        val tup = _root_.cascading.tuple.Tuple.size(${params.size})
-        $set
+    new _root_.com.twitter.scalding.TupleSetter[$T] {
+      override def apply(t: $T): _root_.cascading.tuple.Tuple = {
+        val tup = _root_.cascading.tuple.Tuple.size(${set.size})
+        ..$set
         tup
       }
-      override def arity = ${params.size}
+      override def arity = ${set.size}
     }
     """
     c.Expr[TupleSetter[T]](res)
   }
 
   def caseClassTupleConverter[T]: TupleConverter[T] = macro caseClassTupleConverterImpl[T]
-  def caseClassTupleConverterImpl[T](c: Context)(implicit tag: c.WeakTypeTag[T]): c.Expr[TupleConverter[T]] = {
+  def caseClassTupleConverterImpl[T](c: Context)(implicit T: c.WeakTypeTag[T]): c.Expr[TupleConverter[T]] = {
     import c.universe._
     if (!isCaseClass[T](c)) {
-      throw new IllegalArgumentException("Type paramter of caseClassTupleConverter must be a case class")
+      throw new IllegalArgumentException("Type parameter of caseClassTupleConverter must be a case class")
     }
-    val params = tag.tpe.declaration(nme.CONSTRUCTOR).asMethod.paramss.head
-    val gets =
-      params
+    //TODO go over it recursively to get the caseClassTupleConverters that we need. May make more sense
+    // to return it when we go over it in this case?
+    val get =
+      T.tpe.declarations
+        .collect { case m: MethodSymbol if m.isCaseAccessor => m.returnType }
         .zipWithIndex
         .map {
-          case (sym, idx) =>
-            val name = newTermName(sym.name.toString)
-            sym.typeSignature match {
+          case (returnType, idx) =>
+            returnType match {
               case tpe if tpe =:= typeOf[String] => q"""tup.getString(${idx})"""
               case tpe if tpe =:= typeOf[Boolean] => q"""tup.getBoolean(${idx})"""
               case tpe if tpe =:= typeOf[Short] => q"""tup.getShort(${idx})"""
@@ -70,7 +68,7 @@ object Macro {
               case tpe if tpe =:= typeOf[Long] => q"""tup.getLong(${idx})"""
               case tpe if tpe =:= typeOf[Float] => q"""tup.getFloat(${idx})"""
               case tpe if tpe =:= typeOf[Double] => q"""tup.getDouble(${idx})"""
-              case tpe if tpe.typeSymbol.asClass.isCaseClass =>
+              case tpe if isCaseClassType(c)(tpe) =>
                 q"""
                 caseClassTupleConverter[${tpe}](
                   new _root_.cascading.tuple.TupleEntry(tup.getObject(${idx})
@@ -82,17 +80,19 @@ object Macro {
         }
 
     val res = q"""
-    new _root_.com.twitter.scalding.TupleConverter[${tag.tpe}] {
-      override def apply(t: _root_.cascading.tuple.TupleEntry): ${tag.tpe} = {
+    new _root_.com.twitter.scalding.TupleConverter[$T] {
+      override def apply(t: _root_.cascading.tuple.TupleEntry): $T = {
         val tup = t.getTuple()
-        ${tag.tpe.typeSymbol.companionSymbol}(..$gets)
+        ${T.tpe.typeSymbol.companionSymbol}(..$get)
       }
-      override def arity = ${params.size}
+      override def arity = ${get.size}
     }
     """
     c.Expr[TupleConverter[T]](res)
   }
 
-  def isCaseClass[T](c: Context)(implicit tag: c.WeakTypeTag[T]): Boolean =
-    BasicTry { tag.tpe.typeSymbol.asClass.isCaseClass }.toOption.getOrElse(false)
+  def isCaseClass[T](c: Context)(implicit T: c.WeakTypeTag[T]): Boolean = isCaseClassType(c)(T.tpe)
+
+  def isCaseClassType(c: Context)(tpe: c.universe.Type): Boolean =
+    BasicTry { tpe.typeSymbol.asClass.isCaseClass }.toOption.getOrElse(false)
 }
